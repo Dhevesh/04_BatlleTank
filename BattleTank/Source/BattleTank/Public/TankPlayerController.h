bf9bd9647fa4fb2e18a8ec16a98c4c040a0ebd34
IMS\Dheveshp <dheveshp@gmail.com>
2019-01-09 16:14:58 +0000

    BT24 - Using DeprojectScreenToWorld

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -32,6 +32,8 @@ private:
 
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 
+	bool GetLookDirection(FVector2D ScreenLocation, FVector & LookDirection) const;
+
 	UPROPERTY(EditAnywhere)
 	float CrossHairXLocation = 0.5;
 
